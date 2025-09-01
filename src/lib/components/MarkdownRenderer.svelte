2641e24eac55c5b8cd67433bc0c2704663be482c
Alexey <mr.swasher@gmail.com>
2025-09-01 20:29:11 +0300

    fix

--- src/lib/components/MarkdownRenderer.svelte
+++ src/lib/components/MarkdownRenderer.svelte
@@ -2,8 +2,6 @@
 	import { marked } from 'marked';
 	import DOMPurify from 'dompurify';
 
-	
-
 	// Пропс 'text' - это строка markdown из Firebase
 	let { text = '' } = $props();
 
@@ -26,38 +24,3 @@
 {:else}
 	<p>Загрузка...</p>
 {/if}
-
-
-
------------- Вариант Grok
-
-
-
-<div class="flex items-center w-full max-w-md mx-auto">
-	<div class="relative w-full flex items-center">
-		<!-- Поисковая строка -->
-		<Search
-			size="lg"
-			placeholder="Поиск..."
-			classes={{ input: "rounded-lg pr-20 pl-10" }}
-		>
-			<!-- Иконка поиска -->
-			<SearchOutline class="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-500" />
-			<!-- Переключатель внутри поля ввода -->
-			<div class="absolute right-3 top-1/2 transform -translate-y-1/2">
-				<Toggle size="small" color="purple" checked={false}>
-					{#snippet offLabel()}Нет{/snippet}
-					Да
-				</Toggle>
-			</div>
-		</Search>
-	</div>
-</div>
-
-<style>
-    /* Дополнительные стили для тонкой настройки, если нужно */
-    :global(.flowbite-toggle) {
-        /* Уменьшаем размер переключателя для гармоничного встраивания */
-        transform: scale(0.8);
-    }
-</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.css';
-	import Navbar from '$lib/components/navbar.svelte';
+	import Navbar from '$lib/components/Header.svelte';
 	import Auth from '$lib/components/Auth.svelte';
 	import { user } from '$lib/firebase'; // Импортируем только user
 	import { Spinner } from 'flowbite-svelte';
